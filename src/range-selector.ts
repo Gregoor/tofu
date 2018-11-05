527a6ee4184b42a9eace2fbfb5e216ecfd99383d
Gregor Weber <gregorwbr@gmail.com>
2018-11-05 23:32:39 +0100

    extend selection to previous/next element

--- src/range-selector.ts
+++ src/range-selector.ts
@@ -1,11 +1,16 @@
 const t = require('@babel/types');
-import moveCursor, { Cursor, Direction } from './move-cursor';
+import moveCursor, { Cursor, Direction, spreadCursor } from './move-cursor';
 import { getFocusPath, getNode } from './ast-utils';
 
 function cursorFromNode(ancestor): Cursor {
   return Array.isArray(ancestor)
     ? [ancestor[0].start, ancestor[ancestor.length - 1].end]
     : [ancestor.start, ancestor.end];
+}
+
+function findIndexForCursor(collection, cursor: number | [number, number]) {
+  const [start, end] = spreadCursor(cursor);
+  return collection.findIndex(node => node.start <= start && node.end >= end);
 }
 
 export default class RangeSelector {
@@ -39,19 +44,59 @@ export default class RangeSelector {
       return cursor;
     }
 
+    const parents = getFocusPath(ast, cursor[0])[0].reverse();
+    const node = getNode(ast, cursor[0]);
+
+    const isRight = direction == 'RIGHT';
     const nextCursor = moveCursor(
       ast,
       code,
       direction,
-      direction == 'RIGHT' ? Math.max(...cursor) : Math.min(...cursor)
+      isRight ? Math.max(...cursor) : Math.min(...cursor)
     );
     const nextNode = getNode(ast, nextCursor[0]);
 
-    return (t.isLiteral(nextNode) || t.isIdentifier(nextNode)) &&
+    if (
+      node == nextNode &&
+      (t.isLiteral(nextNode) || t.isIdentifier(nextNode)) &&
       nextNode.start < nextCursor[0] &&
       nextNode.end > nextCursor[1]
-      ? [Math.min(cursor[0], nextCursor[0]), Math.max(cursor[1], nextCursor[1])]
-      : [Math.min(nextNode.start, getNode(ast, cursor[0]).start), nextNode.end];
+    ) {
+      return [
+        Math.min(cursor[0], nextCursor[0]),
+        Math.max(cursor[1], nextCursor[1])
+      ];
+    } else {
+      const collection = parents.find(node => Array.isArray(node));
+      if (!collection) return cursor;
+
+      const [initialIndex, startIndex, endIndex] = [
+        this.initialRange,
+        cursor[0],
+        cursor[1]
+      ].map(findIndexForCursor.bind(null, collection));
+
+      let nextStartIndex, nextEndIndex;
+      if (isRight) {
+        if (startIndex == initialIndex) {
+          nextStartIndex = startIndex;
+          nextEndIndex = Math.min(endIndex + 1, collection.length - 1);
+        } else {
+          nextStartIndex = startIndex + 1;
+          nextEndIndex = endIndex;
+        }
+      } else {
+        if (endIndex == initialIndex) {
+          nextStartIndex = Math.max(startIndex - 1, 0);
+          nextEndIndex = endIndex;
+        } else {
+          nextStartIndex = startIndex;
+          nextEndIndex = endIndex - 1;
+        }
+      }
+
+      return [collection[nextStartIndex].start, collection[nextEndIndex].end];
+    }
   }
 
   reset() {
