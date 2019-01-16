a12639dba77742ca0b0f551db887fa7e9ace6903
Gregor Weber <gregorwbr@gmail.com>
2019-01-16 23:00:41 +0100

    make demo code make "sense"

--- src/demo.tsx
+++ src/demo.tsx
@@ -4,9 +4,9 @@ import Editor from './editor';
 
 const WELCOME_CODE = [
   "const FEATURES = ['Edit code with few keystrokes', 'No syntax errors', 'Formatted code, no action required']",
-  "const TODOS = ['Support all the JS language features']",
+  "const TODOS = ['Support all the JS language features', 'Squash all dem bugs']",
   '',
-  'if (FEATURES.length > TODOS.length) { release(); }'
+  'if (FEATURES.length > 10 * TODOS.length) { release(); }'
 ].join('\n');
 
 render(<Editor code={WELCOME_CODE} />, document.querySelector('#editor'));
