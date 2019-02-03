3a185473cefc66c474f3347ea97575ebe7eebc9c
Gregor Weber <gregorwbr@gmail.com>
2019-02-04 00:37:31 +0100

    make EditorState readonly

--- src/edtior-state.ts
+++ src/edtior-state.ts
@@ -1,10 +1,10 @@
 import { Cursor } from './move-cursor';
 
 type AST = { type: string, start: number, end: number };
-export type EditorState = {
+export type EditorState = Readonly<{
   ast: null | AST;
   lastValidAST: AST;
   code: string;
   printWidth: number;
   cursor: Cursor;
-};
+}>;
