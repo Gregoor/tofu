437edb6e1e8a4befaf82733f751a0b3c02307790
Gregor Weber <gregorwbr@gmail.com>
2019-02-08 21:39:19 +0100

    keymap as functional component

--- src/keymap.tsx
+++ src/keymap.tsx
@@ -10,111 +10,101 @@ import {
   SectionTitle
 } from './ui';
 
-export default class Keymap extends React.Component<{
+export default function Keymap(props: {
   actions: any;
   onExecute: (action: { execute: Action }) => any;
   onSearchBlur: () => any;
   searchIn: string;
   searchRef: React.RefObject<HTMLInputElement>;
-}> {
-  render() {
-    const {
-      actions,
-      onExecute,
-      onSearchBlur,
-      searchIn,
-      searchRef
-    } = this.props;
+}) {
+  const { actions, onExecute, onSearchBlur, searchIn, searchRef } = props;
 
-    return actions
-      .filter(a => a.title)
-      .map(({ title, alt, ctrl, shift, key, children }, i) => (
-        <ActionSection key={i}>
-          <Downshift
-            defaultHighlightedIndex={0}
-            onChange={selection =>
-              onExecute(children.find(a => a.name == selection))
-            }
-          >
-            {({
-              getInputProps,
-              getItemProps,
-              getLabelProps,
-              getMenuProps,
-              inputValue,
-              highlightedIndex
-            }) => (
-              <div>
-                {searchIn == title ? (
-                  <input
-                    {...getInputProps({
-                      type: 'text',
-                      placeholder: `Search "${title}"`,
-                      ref: searchRef,
-                      onBlur: onSearchBlur
-                    })}
-                  />
-                ) : (
-                  <SectionTitle>
-                    {title}
-                    <div>
-                      {alt && <Key>Alt</Key>}
-                      {ctrl && <Key>Ctrl</Key>}
-                      {shift && <Key>Shift</Key>}
-                      {key && <Key>{key}</Key>}
-                    </div>
-                  </SectionTitle>
-                )}
-                <ActionList {...getMenuProps()}>
-                  {children
-                    .filter(
-                      a =>
-                        searchIn != title ||
-                        a.name.toLowerCase().includes(inputValue.toLowerCase())
-                    )
-                    .map((action, i) => {
-                      const keys = [];
-                      if (action.key) {
-                        keys.push(action.key);
-                      }
-                      if (action.codes) {
-                        keys.push(
-                          ...action.codes.map(
-                            code => ({ Comma: ',' }[code] || code)
-                          )
-                        );
-                      }
-                      return (
-                        <ActionItem
-                          {...getItemProps({
-                            key: i,
-                            item: action.name
-                          })}
-                          highlighted={
-                            searchIn == title && highlightedIndex == i
-                          }
-                        >
-                          <div>{action.name}</div>
-                          <Keys>
-                            {keys.map(key => (
-                              <Key key={key}>
-                                {{
-                                  ArrowLeft: '⬅',
-                                  ArrowRight: '➡',
-                                  ArrowDown: '⬇',
-                                  ArrowUp: '⬆'
-                                }[key] || key}
-                              </Key>
-                            ))}
-                          </Keys>
-                        </ActionItem>
+  return actions
+    .filter(a => a.title)
+    .map(({ title, alt, ctrl, shift, key, children }, i) => (
+      <ActionSection key={i}>
+        <Downshift
+          defaultHighlightedIndex={0}
+          onChange={selection =>
+            onExecute(children.find(a => a.name == selection))
+          }
+        >
+          {({
+            getInputProps,
+            getItemProps,
+            getLabelProps,
+            getMenuProps,
+            inputValue,
+            highlightedIndex
+          }) => (
+            <div>
+              {searchIn == title ? (
+                <input
+                  {...getInputProps({
+                    type: 'text',
+                    placeholder: `Search "${title}"`,
+                    ref: searchRef,
+                    onBlur: onSearchBlur
+                  })}
+                />
+              ) : (
+                <SectionTitle>
+                  {title}
+                  <div>
+                    {alt && <Key>Alt</Key>}
+                    {ctrl && <Key>Ctrl</Key>}
+                    {shift && <Key>Shift</Key>}
+                    {key && <Key>{key}</Key>}
+                  </div>
+                </SectionTitle>
+              )}
+              <ActionList {...getMenuProps()}>
+                {children
+                  .filter(
+                    a =>
+                      searchIn != title ||
+                      a.name.toLowerCase().includes(inputValue.toLowerCase())
+                  )
+                  .map((action, i) => {
+                    const keys = [];
+                    if (action.key) {
+                      keys.push(action.key);
+                    }
+                    if (action.codes) {
+                      keys.push(
+                        ...action.codes.map(
+                          code => ({ Comma: ',' }[code] || code)
+                        )
                       );
-                    })}
-                </ActionList>
-              </div>
-            )}
-          </Downshift>
-        </ActionSection>
-      ));
-  }
+                    }
+                    return (
+                      <ActionItem
+                        {...getItemProps({
+                          key: i,
+                          item: action.name
+                        })}
+                        highlighted={searchIn == title && highlightedIndex == i}
+                      >
+                        <div>{action.name}</div>
+                        <Keys>
+                          {keys.map(key => (
+                            <Key key={key}>
+                              {{
+                                ArrowLeft: '⬅',
+                                ArrowRight: '➡',
+                                ArrowDown: '⬇',
+                                ArrowUp: '⬆'
+                              }[key] || key}
+                            </Key>
+                          ))}
+                        </Keys>
+                      </ActionItem>
+                    );
+                  })}
+              </ActionList>
+            </div>
+          )}
+        </Downshift>
+      </ActionSection>
+    ));
 }
